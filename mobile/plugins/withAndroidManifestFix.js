3d20e294f5965964f7cb288a9a3583c3590f9b8b
La Tasa Dev <dev@latasa.local>
2026-02-06 23:28:14 -0400

    Fix: Mobile build configuration (slug & plugins)

--- mobile/plugins/withAndroidManifestFix.js
+++ mobile/plugins/withAndroidManifestFix.js
@@ -1,26 +1,26 @@
-const { withAndroidManifest } = require('expo/config-plugins');
+const { withAndroidManifest } = require('@expo/config-plugins');
 
 module.exports = function withAndroidManifestFix(config) {
   return withAndroidManifest(config, (config) => {
     const androidManifest = config.modResults;
-    
+
     // Ensure tools namespace
     if (!androidManifest.manifest.$['xmlns:tools']) {
-        androidManifest.manifest.$['xmlns:tools'] = "http://schemas.android.com/tools";
+      androidManifest.manifest.$['xmlns:tools'] = "http://schemas.android.com/tools";
     }
 
     const mainApplication = androidManifest.manifest.application[0];
-    
+
     if (mainApplication && mainApplication['meta-data']) {
       const metaData = mainApplication['meta-data'].find(
         (md) => md.$['android:name'] === 'com.google.android.gms.ads.DELAY_APP_MEASUREMENT_INIT'
       );
-      
+
       if (metaData) {
         metaData.$['tools:replace'] = 'android:value';
       }
     }
-    
+
     return config;
   });
 };
